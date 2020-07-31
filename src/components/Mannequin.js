0fb1fc48a8be1a1f917a6733e1ae5777b165da4a
byoon97 <byoon5397@gmail.com>
2020-07-31 13:03:53 -0400

    idk

--- src/components/Mannequin.js
+++ src/components/Mannequin.js
@@ -7,18 +7,18 @@ export default class Mannequin extends React.Component {
         <div className="w-full md:w-1/2 px-4 mb-4 md:mb-0">
           <div className="py-12 px-4">
             <h2 className="text-4xl text-center mb-4 font-heading">OOTD</h2>
-            <p className="max-w-2xl mb-8 mx-auto text-center text-gray-500 leading-relaxed">
+            <p className="max-w-2xl mb-8 mx-auto text-center text-black-500 leading-relaxed">
               Name: {this.props.name}
             </p>
-            <p className="max-w-2xl mb-8 mx-auto text-center text-gray-500 leading-relaxed">
+            <p className="max-w-2xl mb-8 mx-auto text-center text-black-500 leading-relaxed">
               Category: {this.props.category}
             </p>
-            <p className="max-w-2xl mb-8 mx-auto text-center text-gray-500 leading-relaxed">
+            <p className="max-w-2xl mb-8 mx-auto text-center text-black-500 leading-relaxed">
               {this.props.weekday}
             </p>
             <div className="flex flex-wrap -mx-4 -mb-8">
               {this.props.clothes.map((item) => (
-                <div className="md:w-1/3 px-4 mb-8">
+                <div className="md:w-1/2 px-4 mb-8">
                   <img className="rounded shadow-md" src={item.image} alt="" />
                 </div>
               ))}
